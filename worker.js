80c8678df20ab7612863fd68b4d5e3b42dfbaf75
Lennart Kats <lclkats@gmail.com>
2014-05-12 17:18:06 +0200

    Add missing cleanup statement

--- worker.js
+++ worker.js
@@ -1075,6 +1075,7 @@ function endTime(t, message, indent) {
         updateAgain = null;
         clearTimeout(updateWatchDog);
         clearTimeout(this.scheduledUpdate);
+        this.scheduledUpdate = null;
 
         if (!DEBUG) {
             updateWatchDog = setTimeout(function() {
