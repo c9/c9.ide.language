bf7c02e381fd8c4c84d40b5389e7067e72f2df5c
Lennart Kats <lclkats@gmail.com>
2015-12-29 11:04:45 +0100

    Be even more paranoid

--- worker.js
+++ worker.js
@@ -1638,7 +1638,8 @@ function endTime(t, message, indent) {
                 var cache = _self.completionPrediction = _self.getCompleteCacheKey(predictedPos, identifierRegex, predictedLine);
                 _self.getCompleteHandlerResult(event, predictedPos, identifierRegex, predictedLine, function(result) {
                     cache.result = result;
-                    if (showEarly && _self.completionCache.result && cacheKey.matches(_self.completionCache))
+                    var latest = _self.completionCache;
+                    if (showEarly && latest && latest.result && cacheKey.matches(latest))
                         showPredictionsEarly(result);
                 });
             }
